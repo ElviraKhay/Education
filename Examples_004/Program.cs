3d0c9666aff83d9cda3211617907c2cd33c94311
Elvira <elvira.khay@gmail.com>
2022-11-30 16:24:33 +0300

    Random

--- Examples_004/Program.cs
+++ Examples_004/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 119;
-int numberB = 10;
+﻿int numberA = new Random(). Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random(). Next(1,10);
+Console.WriteLine(numberB);
 int Sum = numberA + numberB;
 Console.WriteLine(Sum);

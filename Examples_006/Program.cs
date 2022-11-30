858854350ee298a593c9b2a743c243f6fc4f4fa8
Elvira <elvira.khay@gmail.com>
2022-11-30 16:57:46 +0300

    Тимурчик

---
+++ Examples_006/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "тимур")
+{
+Console.WriteLine("Привет, Тимурчик!");
+
+}
+
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username); 
+    
+}

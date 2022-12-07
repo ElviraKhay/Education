9e499617867bf0c0fc6a887a4f778570940982a0
Elvira <elvira.khay@gmail.com>
2022-12-07 23:00:22 +0300

    задача №1

---
+++ Example_task1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите первое число:  ");
+string inputvalue1 = Console.ReadLine();
+int value1 = Convert.ToInt32(inputvalue1);
+Console.Write("Введите число 2:  ");
+string inputevalue2 = Console.ReadLine();
+int value2 = Convert.ToInt32(inputevalue2);
+
+if (value1 > value2)
+
+{
+  Console.WriteLine("Максимальное число  = " + value1);  
+}
+else 
+{
+
+Console.WriteLine("Минимальное число  = " + value1); 
+
+}

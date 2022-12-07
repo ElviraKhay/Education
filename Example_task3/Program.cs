27a7b416c011cc0d27f821ca0295155cd1a8bb27
Elvira <elvira.khay@gmail.com>
2022-12-07 23:04:05 +0300

    задача №3

---
+++ Example_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число:  ");
+string inputvalue1 = Console.ReadLine();
+int value = Convert.ToInt32(inputvalue1);
+if (value%2==0)
+{
+
+   Console.WriteLine(value + "  четное число"); 
+
+}
+
+else 
+{
+
+    Console.WriteLine(value + "  нечетное число"); 
+
+}
